002542cb1d9ce2d3b761c69678373be54fa243e6
Khalil Stemmler <metroidman12@gmail.com>
2018-09-11 14:33:19 -0400

    Stubs in Angular.
    - Wrote first stub in Angular with FriendsService.

--- src/app/components/friends/friends.component.spec.ts
+++ src/app/components/friends/friends.component.spec.ts
@@ -1,10 +1,17 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed, getTestBed } from "@angular/core/testing";
 import { SearchFriendsPipe } from "../../search-friends.pipe";
 
 import { FriendsComponent } from "./friends.component";
 import { Component, Input } from "@angular/core";
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from "selenium-webdriver/http";
+import { FriendsService } from "../../core/friends.service";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Friend } from "../../shared/friend";
+import { Conversation } from '../../shared/conversation';
+import { Observable, of } from "rxjs";
+
 @Component({
   selector: "app-navigation",
   template: ""
@@ -19,9 +26,36 @@ class MockAppConversationComponent {
   @Input('conversation') conversation;
 }
 
+const testData = [
+  new Friend('stemmllerjs', [], 'https://google.com', false),
+  new Friend('joshhomme', [], 'https://google.com', false),
+  new Friend('hellokitty', [], 'https://google.com', false),
+  new Friend('slight', [], 'https://google.com', false),
+  new Friend('hmz.js', [], 'https://google.com', false)
+];
+
+const conversationData = [
+  new Conversation(new Friend('joshhomme', [], 'https://google.com', false), [])
+];
+
+// FriendsServiceStub
+// This is a stub that is used to replace the FriendsService.
+export class FriendsServiceStub {
+  public getFriends(): Observable<Friend[]> {
+    return of(testData);
+  }
+
+  public getConversation (): Observable<Conversation []> {
+    return of(conversationData);
+  }
+}
+
 describe("FriendsComponent", () => {
   let component: FriendsComponent;
   let fixture: ComponentFixture<FriendsComponent>;
+
+  let injector: TestBed;
+  let service: FriendsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -31,8 +65,18 @@ describe("FriendsComponent", () => {
         SearchFriendsPipe,
         MockAppConversationComponent
       ],
-      imports: [FormsModule, HttpClientModule]
+      imports: [FormsModule, HttpClientModule],
+      providers: [
+        {
+          provide: FriendsService,
+          useClass: FriendsServiceStub
+        }
+      ]
     }).compileComponents();
+
+    // Mock our FriendsService dependency and our Http dependency
+    injector = getTestBed();
+    service = injector.get(FriendsService);
   }));
 
   beforeEach(() => {
@@ -43,5 +87,16 @@ describe("FriendsComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return an Observable<Friend[]>', () => {
+    service.getFriends().subscribe(friends => {
+      expect(friends.length).toBe(5);
+      expect(friends).toEqual(testData);
+    });
+  });
+
+  xit('should switch to the first conversation if there are conversations', () => {
+
   });
 });
--- src/app/search-friends.pipe.spec.ts
+++ src/app/search-friends.pipe.spec.ts
@@ -1,8 +1,31 @@
 import { SearchFriendsPipe } from './search-friends.pipe';
+import { Friend } from './shared/friend';
 
 describe('SearchFriendsPipe', () => {
+  let friends: Friend[];
+  const pipe = new SearchFriendsPipe();
+  beforeAll(() => {
+    friends = [
+      new Friend('stemmllerjs', [], 'https://google.com', false),
+      new Friend('joshhomme', [], 'https://google.com', false),
+      new Friend('hellokitty', [], 'https://google.com', false),
+      new Friend('slight', [], 'https://google.com', false),
+      new Friend('hmz.js', [], 'https://google.com', false)
+    ];
+  });
   it('create an instance', () => {
-    const pipe = new SearchFriendsPipe();
     expect(pipe).toBeTruthy();
+  });
+  it('can filter and return a user by their username', () => {
+    const filtered = pipe.transform(friends, 'stemml');
+    expect(filtered.length).toBe(1);
+  });
+  it('should return none if it doesnt match any names', () => {
+    const filtered = pipe.transform(friends, 'fsddgrdgdrgdhfggh');
+    expect(filtered.length).toBe(0);
+  });
+  it('should be able to locate substrings of matching text', () => {
+    const filtered = pipe.transform(friends, 'll');
+    expect(filtered.length).toBe(2);
   });
 });
