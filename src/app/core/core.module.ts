37dae14ea756e58002e8d0f193be5818783b7c63
Khalil Stemmler <metroidman12@gmail.com>
2018-09-14 13:42:50 -0400

    Added background pattern.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,14 +5,14 @@ import { UserInfoService } from './user-info.service';
 
 /*
  * https://medium.com/@amcdnl/organizing-your-angular-application-2694ff67d335
- * 
+ *
  * For non-shared parts of your application that
  * are used by the core of your application use a Core Module.
  * 'This module is where you might define the chrome of your
  * UI (headers, footers, sidebars), cross-cutting concerns like
  * authentication services and guards for your base feature module
  * routes and anything else that isn’t part of a feature and isn’t shared.
- * 
+ *
  * I often call this module ApplicationModule because it’s all the stuff
  * that would go into a normal feature module but this feature happens to
  * be the main application.
